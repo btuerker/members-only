660d5d1b9a2b8d2cdd8f04fb1c4b9e0473b432a4
Alex <nikitin2009@gmail.com>
2019-03-29 16:15:46 +0200

    Add Post model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:one)
+    @post = Post.new(title: "Post title", body: "Body text", user: @user)
+  end
 
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "should have an associated user" do
+    assert @post.user
+  end
 end
