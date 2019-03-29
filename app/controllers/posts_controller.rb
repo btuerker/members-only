847f16514d950ce7d3f398bab8c9592072e6ed36
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-03-29 13:36:04 +0300

    Add posts#create, add authentication control to posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,38 @@
 class PostsController < ApplicationController
   include ApplicationHelper
-  
+
   before_action :signed_in, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
   end
 
   def create
-  end
-
-  def index
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:succes] = "Post succesfully created!"
+      redirect_to posts_path
+    else
+      flash.now[:danger] = "There was some problems occured"
+      render 'new'
+    end
   end
 
   private
-    def signed_in
-      redirect_to signin_path unless signed_in?
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def signed_in
+    unless signed_in?
+      flash[:danger] = "Please signin to reach posts#new"
+      redirect_to signin_path
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
+  root 'posts#index'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#delete'
   resources :sessions
